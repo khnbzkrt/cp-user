e4cecc0703859d486d163e4a44c976b65fa91551
khnbzkrt <khan.bozkurt@gmail.com>
2024-10-20 21:35:12 +0300

    allowed origins added to server actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     transpilePackages: ['mui-tel-input'],
+    experimental:{
+        serverActions:{
+            allowedOrigins:['49.12.111.204:3030','49.12.111.204']
+        }
+    }
 };
 
 export default nextConfig;
